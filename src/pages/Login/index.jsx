a16087ef0451c9c044ed573e776a532a3f8ea9e7
meilingfu <1051249847@qq.com>
2024-07-09 23:52:33 +0800

    登录与token管理逻辑

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,22 @@
 import './index.scss'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, message } from 'antd'
 import { fetchLogin } from '@/store/modules/user'
 import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
   //触发 异步请求获取 token
   const dispatch = useDispatch()
-  const onFinish = (value) => {
+  const navigate = useNavigate()
+  const onFinish = async (value) => {
     console.log('表单：', value)
-    dispatch(fetchLogin(value))
+    //获取 token 并存储
+    await dispatch(fetchLogin(value))
+    //跳转到首页
+    navigate('/')
+    //提示用户登录是否成功
+    message.success('登录成功~')
   }
 
   return (
