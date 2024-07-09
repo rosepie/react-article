fb56af0f6dcf8aae0fa1161fd4414d8532706770
meilingfu <1051249847@qq.com>
2024-07-09 09:00:25 +0800

    登录页面静态结构搭建 表单校验

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,55 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+
 const Login = () => {
+  const onFinish = (value) => {
+    console.log('登录表单：', value)
+  }
   return (
-    <div>我是 登录页面</div>
+    <div className="login">
+      <Card className="login-container">
+        {/* 登录表单 */}
+        {/* 业务定制化： 失焦时触发校验*/}
+        <Form validateTrigger="onBlur" onFinish={onFinish}>
+          <Form.Item
+            name="mobile"
+            // 默认：点击登录按钮触发的校验，顺序触发
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号'
+              },
+              {
+                pattern: /^1[3-9]\d{9}/,
+                message: '请输入正确的手机号格式'
+              }
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: '请输入密码'
+              },
+              {
+                pattern: /[0-9a-zA-Z]{9}/,
+                message: '请输入正确格式的密码(9位数,数字或字母)'
+              }
+            ]}
+          >
+            <Input size="large" placeholder="请输入密码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   )
 }
 
