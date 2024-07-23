16603ecb7958c78fd62b3cb0e8ea89c9a3fdc6b2
meilingfu <1051249847@qq.com>
2024-07-23 23:05:44 +0800

    token持久化 注入请求拦截器

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,11 @@
 //统一导出所有工具函数的路径，按需导出
 
 import { request } from './request'
+import { getToken, setToken, removeToken } from './token'
 
 export {
-  request
+  request,
+  getToken,
+  setToken,
+  removeToken
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 // axios封装
 // 1.根域名配置 2.超时时间 3.请求拦截器/响应拦截器
 import axios from 'axios'
+import { getToken } from './token'
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -9,6 +10,11 @@ const request = axios.create({
 
 //请求拦截器：在请求发送之前 做拦截 插入一些自定义配置 重点是参数的处理
 request.interceptors.request.use((config) => {
+  // token 按照后端的格式要求做token拼接
+  const token = getToken()
+  if(token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, (error) => {
   return Promise.reject(error)
