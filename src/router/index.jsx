970e9eac2446e756604540535bc00177c35b1f9b
meilingfu <1051249847@qq.com>
2024-08-03 16:55:24 +0800

    打包优化 路由懒加载

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,33 +1,38 @@
-import Login from '../pages/Login'
-import Layout from '../pages/Layout'
-import Home from '@/pages/Home'
-import Article from '@/pages/Article'
-import Publish from '@/pages/Publish'
 import { createBrowserRouter } from 'react-router-dom'
+import { lazy } from 'react'
+import { Suspense } from 'react'
+
 import AuthRoute from '@/components/AuthRoute'
+
+//打包路由懒加载，react提供的 lazy 函数，配合 react 内置的 Suspense 组件
+const Home = lazy(() => import('@/pages/Home'))
+const Login = lazy(() => import('@/pages/Login'))
+const Layout = lazy(() => import('@/pages/Layout'))
+const Publish = lazy(() => import('@/pages/Publish'))
+const Article = lazy(() => import('@/pages/Article'))
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <AuthRoute><Layout /></AuthRoute>,
+    element: <Suspense fallback={'加载中'}><AuthRoute><Layout /></AuthRoute></Suspense>,
     children: [
       {
         index: true,
-        element: <Home />
+        element: <Suspense fallback = { '加载中' }><Home /></Suspense>
       },
       {
         path: 'article',
-        element: <Article />
+        element: <Suspense fallback={'加载中'}><Article /></Suspense>
       },
       {
         path: 'publish',
-        element: <Publish />
+        element: <Suspense fallback={'加载中'}><Publish /></Suspense>
       }
     ]
   },
   {
     path: '/login',
-    element: <Login />
+    element: <Suspense fallback={'加载中'}><Login /></Suspense>
   }
 ])
 
